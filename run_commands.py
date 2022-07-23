14516e5cb18d322eb28b321dcfca41ef7b013fff
shashank-k-y <shashank.sky.8@gmail.com>
2022-07-23 19:58:57 +0530

    read input from file

--- run_commands.py
+++ run_commands.py
@@ -10,35 +10,30 @@ from functions import (
 
 parking_lot = None
 
-while True:
-    command = str(input("enter the command: ")).split(' ')
+
+def run_command(command, parking_lot=parking_lot):
+    command = command.split(' ')
     if command[0] == 'exit':
-        break
+        return
 
-    if not command[0] == "Create_parking_lot":
-        print("please create a parkinglot first or please check the command")
-        continue
+    if not parking_lot and command[0] == "Create_parking_lot":
+        try:
+            max_slots = int(command[1])
+        except ValueError:
+            print("please give an integer value to the parking slots")
+            return parking_lot
 
-    try:
-        max_slots = int(command[1])
-    except ValueError:
-        print("please give an integer value to the parking slots")
+        parking_lot = create_parking_lot(max_slots=max_slots)
+        print(f'Created parking lot of {max_slots} slots')
+        return parking_lot
 
-    parking_lot = create_parking_lot(max_slots=max_slots)
-    print(f'Created parking of {max_slots} slots')
-    break
-
-while True:
-    command = str(input("Enter the command: ")).split(' ')
-    if command[0] == 'exit':
-        break
-
-    if not parking_lot:
-        print("please create a Parking lot with definate slots")
-        continue
-
-    if command[0] == "Create_parking_lot":
+    elif parking_lot and command[0] == "Create_parking_lot":
         print("parking_lot already created, please use other commands")
+        return parking_lot
+
+    elif not parking_lot and command[0] != "Create_parking_lot":
+        print("please create a parkinglot first or please check the command")
+        return parking_lot
 
     elif command[0] == 'Park':
         register_number = command[1]
@@ -48,7 +43,7 @@ while True:
         free_space = any(slot is None for slot in slot_list)
         if not free_space:
             print("Parking lot is full, please wait.")
-            continue
+            return parking_lot
 
         car_already_exists = check_car_object_already_exists(
             register_number=register_number, slot_list=slot_list
@@ -58,7 +53,7 @@ while True:
                 f"The car with registered number {register_number} " +
                 "already parked"
             )
-            continue
+            return parking_lot
 
         car = allocate_slot_to_car(
             register_number=register_number, driver_age=driver_age,
@@ -68,22 +63,23 @@ while True:
             f"Car with vehicle registration number {car.register_number}" +
             f"has been parked at slot number {car.slot_number}"
         )
+        return parking_lot
 
     elif command[0] == "Slot_numbers_for_driver_of_age":
         try:
             driver_age = int(command[1])
         except ValueError:
             print("driver age should be an integer")
-            continue
+            return parking_lot
 
         slot_numbers = get_slot_numbers_from_driver_age(
             parking_lot=parking_lot, driver_age=driver_age
         )
         if not slot_numbers:
             print(f"No slots found for driver age {driver_age}")
-            continue
 
         print(",".join(str(slot) for slot in slot_numbers))
+        return parking_lot
 
     elif command[0] == "Slot_number_for_car_with_number":
         register_number = command[1]
@@ -93,9 +89,9 @@ while True:
         )
         if not slot_numbers:
             print(f"No slots found for registration number{register_number}")
-            continue
 
         print(",".join(str(slot) for slot in slot_numbers))
+        return parking_lot
 
     elif command[0] == "Leave":
         slot = int(command[1])
@@ -104,7 +100,6 @@ while True:
         )
         if not car_details:
             print(f"no car is parked in slot {slot}")
-            continue
 
         print(
             f"Slot number {slot} vacated, " +
@@ -113,6 +108,7 @@ while True:
             "left the space,the driver of the car was of age" +
             f"{car_details.driver_age}"
         )
+        return parking_lot
 
     elif command[0] == "Vehicle_registration_number_for_driver_of_age":
         driver_age = int(command[1])
@@ -121,14 +117,22 @@ while True:
         )
         if not registration_numbers:
             print(f"no vehicals where found where driver age is {driver_age}")
-            continue
 
         reg_numbers = ','.join(number for number in registration_numbers)
         slot_numbers = ','.join(str(number) for number in slots)
 
         print(
             f"Car with vehicle registration number {reg_numbers} " +
-            f"has been parked at slot number {slot_numbers}")
+            f"has been parked at slot number {slot_numbers}"
+        )
+        return parking_lot
 
     else:
         print("please use accurate commands")
+        return parking_lot
+
+
+with open('input.txt', 'r') as file:
+    lines = file.read().splitlines()
+    for line in lines:
+        parking_lot = run_command(command=line, parking_lot=parking_lot)
