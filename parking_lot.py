c8859064e1e6555720fc7aa32eaa546ba3c9b08d
shashank-k-y <shashank.sky.8@gmail.com>
2022-07-23 16:37:00 +0530

    add string representation method

--- parking_lot.py
+++ parking_lot.py
@@ -9,7 +9,10 @@ class Car():
         self.driver_age = driver_age
         self.slot_number = self.assign_empty_slot(parking_lot=parking_lot) + 1
 
-    def assign_empty_slot(self, parking_lot):
+    def __str__(self) -> str:
+        return f"{self.register_number}, {self.driver_age}, {self.slot_number}"
+
+    def assign_empty_slot(self, parking_lot: object) -> int:
         slots = parking_lot.slot_list
         for i in range(len(slots)):
             if slots[i] is None:
